79ec41fd208040e2a4b5e81fceb940c32c2f5447
akeemseymens <812@holbertonschool.com>
2020-02-07 03:33:46 +0000

    added edge case

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -11,13 +11,14 @@ def recurse(subreddit, hot_list=[], after=None):
     headers = {"User-Agent": "py3"}
     param = {'limit': 100, 'after': after}
     r = requests.get(url, headers=headers, params=param,
-                     allow_redirects=False).json()
+                     allow_redirects=False)
+    if r.status_code != 200:
+        return None
+    r = r.json()
     after = r.get('data').get('after')
     _list_title = list(map(lambda x: x.get('data').get('title'),
                            r.get('data').get('children')))
     hot_list += _list_title
-    if r.status_code not 200:
-        print('None')
     if after is None:
         return hot_list
     else:
