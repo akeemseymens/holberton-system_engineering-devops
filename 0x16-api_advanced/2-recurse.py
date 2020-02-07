e18515fa3505b7a4d6cf8f33c97d96da5261cd03
akeemseymens <812@holbertonschool.com>
2020-02-07 03:25:59 +0000

    bug fix?

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,14 +9,16 @@ def recurse(subreddit, hot_list=[], after=None):
     '''recurse a hotlist of subreddit'''
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {"User-Agent": "py3"}
-    param = {'limit':100,'after':after}
+    param = {'limit': 100, 'after': after}
     r = requests.get(url, headers=headers, params=param,
                      allow_redirects=False).json()
     after = r.get('data').get('after')
     _list_title = list(map(lambda x: x.get('data').get('title'),
-                               r.get('data').get('children')))
+                           r.get('data').get('children')))
     hot_list += _list_title
+    if r.status_code not 200:
+        print('None')
     if after is None:
-         return hot_list
+        return hot_list
     else:
         return recurse(subreddit, hot_list, after)
