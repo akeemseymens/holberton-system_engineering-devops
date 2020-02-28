9c9de52440960afd2140da27234a51f9defb7c52
akeemseymens <812@holbertonschool.com>
2020-02-28 06:14:32 +0000

    added files

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+# change limit for nginx server
+exec { 'upper limit':
+  command => 'sed -i s/15/1000/ /etc/default/nginx',
+  path    => '/bin',
+}
+service { 'nginx':
+    ensure    => running,
+    subscribe => Exec['upper limit'],
+}
